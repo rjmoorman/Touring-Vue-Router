04ba9456003b3d7203a10d56bce75c56a96e948f
ryan.moorman <ryan.moorman@i-labs.tech>
2023-08-30 15:08:18 -0400

    Pagination add

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,15 +2,33 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div>
+      <router-link
+        id="page-prev" 
+        :to="{ name: 'EventList', query: { page: page -1}}"
+        rel="prev"
+        v-if="page !=1"
+      >Prev Page
+      </router-link>
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page+1 }}"
+        rel="next"
+        v-if="hasNextPage"
+      >Next Page</router-link>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import { watchEffect } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard
   },
@@ -20,18 +38,53 @@ export default {
     }
   },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+      watchEffect(()=>{
+        this.event = null
+        EventService.getEvents(2, this.page)
+          .then(response => {
+            this.events = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    })
+  },
+  computed: {
+    hasNextPage(){
+      var totalPages = Math.ceil(this.totalEvents / 2) // 2 is events per page
+
+      // Then check to see if the current page is less than the total pages.
+      return this.page < totalPages
+    }
   }
 }
 </script>
 
 <style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+
 .events {
   display: flex;
   flex-direction: column;
